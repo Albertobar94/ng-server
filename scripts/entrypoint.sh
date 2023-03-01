fc22c745a3edc3c23b8e54a61ac4b3d7b8ca3fb7
Alberto Barboza <albertobarboza94@gmail.com>
2023-03-01 18:45:39 +0100

    Alberto Barboza - Updating docker config

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,2 +1,6 @@
-npm run db:mig-up
+echo "-------------------> Waiting for db to start"
+sleep 5;
+echo "-------------------> Running migrations"
+./node_modules/.bin/typeorm migration:run -d dist/database/db.config.js;
+echo "-------------------> Finished migrations"
 node --perf_basic_prof_only_functions ./dist/main.js
