11c7c66e4dfb1dfeb99290fc876ce848683f552f
Alberto Barboza <albertobarboza94@gmail.com>
2023-03-01 18:45:17 +0100

    Alberto Barboza - Updating repositories

--- src/community-channel/repository/room-participant.repository.ts
+++ src/community-channel/repository/room-participant.repository.ts
@@ -1,6 +1,11 @@
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { Injectable } from "@nestjs/common";
 import { RoomParticipantEntity } from "../entities/room-participant.entity";
+import { InjectDataSource } from "@nestjs/typeorm";
 
 @Injectable()
-export class RoomParticipantRepository extends Repository<RoomParticipantEntity> {}
+export class RoomParticipantRepository extends Repository<RoomParticipantEntity> {
+  constructor(@InjectDataSource() dataSource: DataSource) {
+    super(RoomParticipantEntity, dataSource.createEntityManager());
+  }
+}
--- src/user/repository/user.repository.ts
+++ src/user/repository/user.repository.ts
@@ -1,9 +1,13 @@
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { Injectable } from "@nestjs/common";
 import { UserEntity } from "../entities/user.entity";
+import { InjectDataSource } from "@nestjs/typeorm";
 
 @Injectable()
 export class UserRepository extends Repository<UserEntity> {
+  constructor(@InjectDataSource() dataSource: DataSource) {
+    super(UserEntity, dataSource.createEntityManager());
+  }
   async selectById(id: string): Promise<UserEntity> {
     return this.createQueryBuilder().where({ id }).getOne();
   }
